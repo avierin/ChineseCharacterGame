6cd62dd58bf96dd40b4c26779897df073bcd4859
avierin <a.vierin@gmail.com>
2013-08-01 13:56:33 +0200

    Deleting wrong version of ruby and rails

--- CCGameWeb/config/initializers/session_store.rb
+++
@@ -1,3 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-CCGameWeb::Application.config.session_store :cookie_store, key: '_CCGameWeb_session'
--- CCGameWeb/test/models/chinese_character_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class ChineseCharacterTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
