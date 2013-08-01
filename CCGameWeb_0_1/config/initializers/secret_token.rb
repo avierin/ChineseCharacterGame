1fbbe86462ce2771200581d2c3dfc8492ec2dec5
avierin <a.vierin@gmail.com>
2013-08-01 14:19:37 +0200

    nouvelle version rails

---
+++ CCGameWeb_0_1/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CCGameWeb01::Application.config.secret_key_base = '62929291f31578e336c2480ccff98c441e6e97b9b849da3ea81c3c79bf8bb8b083764d29d7352be4480b4b35d8fa2ce3da0d218ff7ab61d0a722d9e141262bcc'
